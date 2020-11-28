4db2a6c7c7bc80b6b388fd98b9712061df0a4d89
Janis Kikans <janis.kikans@gmail.com>
2020-11-28 20:22:21 +0200

    initial setup

---
+++ vue.config.js
@@ -0,0 +1,14 @@
+const path = require('path');
+
+module.exports = {
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@api': path.resolve(__dirname, 'src/api'),
+        '@assets': path.resolve(__dirname, 'src/assets'),
+        '@components': path.resolve(__dirname, 'src/components'),
+        '@views': path.resolve(__dirname, 'src/views'),
+      },
+    },
+  },
+};
