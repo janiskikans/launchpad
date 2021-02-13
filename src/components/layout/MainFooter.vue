d7d3a5a088f54454d3d4ed788fe65c612e91e5f5
Janis Kikans <janis.kikans@gmail.com>
2021-02-13 18:35:30 +0200

    use ion-icons instead of font-awesome

--- src/components/layout/MainFooter.vue
+++ src/components/layout/MainFooter.vue
@@ -11,7 +11,7 @@
           target="_blank"
           rel="noopener"
         >
-          <i class="fab fa-github"></i>
+          <ion-icon name="logo-github" />
         </a>
         <a
           class="text-gray-500 hover:text-gray-300"
@@ -19,7 +19,7 @@
           target="_blank"
           rel="noopener"
         >
-          <i class="fab fa-linkedin"></i>
+          <ion-icon name="logo-linkedin" />
         </a>
       </div>
     </footer>
@@ -31,3 +31,9 @@ export default {
   name: 'MainFooter',
 };
 </script>
+
+<style lang="scss" scoped>
+ion-icon {
+  font-size: 17px;
+}
+</style>
